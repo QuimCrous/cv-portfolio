adb9f35486b35219f6867de707e38dfbf67299b3
Quim Crous <joaquimcrous@gmail.com>
2023-03-08 15:13:12 +0100

    First commit

---
+++ app.vue
@@ -0,0 +1,52 @@
+import Pages from './pages/index.vue';
+
+<template>
+  <Navigation />
+  <div>
+    <Index />
+  </div>
+</template>
+
+<script setup>
+import Index from "./pages/index.vue";
+import Navigation from "./pages/header.vue";
+useHead({
+  script: [
+    {
+      src: "/assets/js/jquery-3.6.0.min.js",
+      body: true,
+    },
+    { src: "/assets/js/bootstrap.bundle.min.js", body: true },
+    { src: "/assets/js/jquery.appear.js", body: true },
+    { src: "/assets/js/jquery.easing.min.js", body: true },
+    { src: "/assets/js/jquery.magnific-popup.min.js", body: true },
+    { src: "/assets/js/modernizr.custom.13711.js", body: true },
+    { src: "/assets/js/swiper-bundle.min.js", body: true },
+    { src: "/assets/js/wow.min.js", body: true },
+    { src: "/assets/js/progress-bar.min.js", body: true },
+    { src: "/assets/js/circle-progress.js", body: true },
+    { src: "/assets/js/isotope.pkgd.min.js", body: true },
+    { src: "/assets/js/imagesloaded.pkgd.min.js", body: true },
+    { src: "/assets/js/jquery.nice-select.min.js", body: true },
+    { src: "/assets/js/count-to.js", body: true },
+    { src: "/assets/js/jquery.scrolla.min.js", body: true },
+    { src: "/assets/js/YTPlayer.min.js", body: true },
+    { src: "/assets/js/TweenMax.min.js", body: true },
+    { src: "/assets/js/typed.js", body: true },
+    { src: "/assets/js/validnavs.js", body: true },
+    { src: "/assets/js/main.js", body: true },
+  ],
+});
+</script>
+
+<script>
+export default {
+  mounted: function () {},
+};
+// export default {
+//   plugins: [
+//     "/assets/js/jquery-3.6.0.min.js",
+//
+//   ],
+// };
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: [
+    "/assets/css/bootstrap.min.css",
+    "/assets/css/font-awesome.min.css",
+    "/assets/css/themify-icons.css",
+    "/assets/css/elegant-icons.css",
+    "/assets/css/flaticon-set.css",
+    "/assets/css/magnific-popup.css",
+    "/assets/css/swiper-bundle.min.css",
+    "/assets/css/animate.css",
+    "/assets/css/validnavs.css",
+    "/assets/css/helper.css",
+    "/assets/css/unit-test.css",
+    "/assets/css/style.css",
+  ],
+});
