121c1da39d946115911585c661deb80314945bf6
Quim Crous <joaquimcrous@gmail.com>
2023-03-13 11:26:17 +0100

    solved small bug

--- components/banner.vue
+++ components/banner.vue
@@ -16,7 +16,7 @@
                 <!-- type headline start-->
                 <span class="cd-headline clip is-full-width">
                   <!-- ROTATING TEXT -->
-                  <span class="cd-words-wrapper">
+                  <span class="cd-words-wrapper" :key="key">
                     <b class="is-visible"
                       >Fullstack Developer and 3D designer</b
                     >
@@ -68,6 +68,20 @@
   <!-- End Banner -->
 </template>
 
-<script setup></script>
+<script>
+export default {
+  data: function () {
+    return {
+      key: Math.random(),
+    };
+  },
+  mounted: function () {
+    console.log(this);
+    jQuery(document).ready(function (d) {
+      initTyping(d);
+    });
+  },
+};
+</script>
 
 <style scoped></style>
